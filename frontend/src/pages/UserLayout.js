08eaee05549b36372e9da74d8da67f035a7c743e
Farseel <farseel07@gmail.com>
2026-05-06 16:39:49 +0530

    change 10

--- frontend/src/pages/UserLayout.js
+++ frontend/src/pages/UserLayout.js
@@ -1,7 +1,6 @@
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Products from "./Products";
 import Cart from "./Cart";
-import DealsUser from "./DealsUser";
 
 export default function UserLayout() {
   return (
@@ -10,14 +9,12 @@ export default function UserLayout() {
         <Link to="/">Home</Link>
         <Link to="/user/products">Products</Link>
         <Link to="/user/cart">Cart</Link>
-        <Link to="/user/deals">Deals</Link>
       </nav>
 
       <Routes>
         <Route index element={<Navigate to="products" replace />} />
         <Route path="products" element={<Products />} />
         <Route path="cart" element={<Cart />} />
-        <Route path="deals" element={<DealsUser />} />
       </Routes>
     </div>
   );
